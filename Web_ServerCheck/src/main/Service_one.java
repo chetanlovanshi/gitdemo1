ecfc8bfd70bf2445452c903e7a887f7ff11a7364
chetanlovanshi <clc.lovanshi@gmail.com>
2015-11-24 10:29:32 +0530

    updation

--- Web_ServerCheck/src/main/Service_one.java
+++ Web_ServerCheck/src/main/Service_one.java
@@ -5,7 +5,8 @@ public class Service_one
 	public void sayHello()
 	{
 		System.out.println("Hello");
-		System.out.println("Made in Git")
+		System.out.println("Made in Git");
+		System.out.println("added a second test output");
 	}
 
 }
